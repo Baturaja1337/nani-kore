9408fbf2edb18698da17009386e30af7ed9c00b2
_reduce <61683211+r3duc3@users.noreply.github.com>
2020-07-23 02:19:49 +0000

    Update login.php

--- login.php
+++ login.php
@@ -4,6 +4,10 @@
 	<title>Login</title>
 </head>
 <body>
-
+<form method="POST">
+	<input type="text" name="user"><br>
+	<input type="password" name="pass"><br>
+	<input type="submit" name="check" value="login">
+</form>
 </body>
 </html>
